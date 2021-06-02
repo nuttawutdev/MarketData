6f94f8d11d8143f72f5df7c7ea77b618d4f7d710
Nuttawut <developernuttawut@gmail.com>
2021-06-02 22:37:39 +0700

    login and home page

--- MarketData/Controllers/HomeController.cs
+++ MarketData/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace MarketData.Controllers
         {
             return View();
         }
-
+        public IActionResult Login()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
