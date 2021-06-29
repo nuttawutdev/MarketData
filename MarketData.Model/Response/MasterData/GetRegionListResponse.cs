e81fbc9ec34c760952a7ed19a315d30f10421f8e
Nuttawut <developernuttawut@gmail.com>
2021-06-29 20:08:37 +0700

    Add basr response

--- MarketData.Model/Response/MasterData/GetRegionListResponse.cs
+++ MarketData.Model/Response/MasterData/GetRegionListResponse.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace MarketData.Model.Response.MasterData
 {
-    public class GetRegionListResponse
+    public class GetRegionListResponse : BaseResponse
     {
         public List<RegionData> data { get; set; }
     }
