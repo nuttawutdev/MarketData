d47425e62abe17f4153934dccf9613f68ac7d9b7
Nuttawut <developernuttawut@gmail.com>
2021-08-03 23:48:07 +0700

    บันทึกข้อมูลยอดขายรายร้านค้า

--- MarketData/Controllers/KeyInController.cs
+++ MarketData/Controllers/KeyInController.cs
@@ -1,9 +1,11 @@
-﻿using MarketData.Processes;
+﻿using MarketData.Model.Response.KeyIn;
+using MarketData.Processes;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarketData.Models;
 
 namespace MarketData.Controllers
 {
@@ -34,7 +36,37 @@ namespace MarketData.Controllers
             return View();
         }
 
+        #region BA KeyIn Function
+        //[HttpPost]
+        //public IActionResult GetBAKeyInList(Guid userID)
+        //{
+            //BAKeyInListViewModel BAKeyInListView = new BAKeyInListViewModel();
 
+            //if (ModelState.IsValid)
+            //{
+            //    var response = process.masterData.GetBrandList();
+
+            //    if (response != null && response.data != null && response.data.Any())
+            //    {
+                  
+            //    }
+            //    else
+            //    {
+            //        BAKeyInListView.brandList = new List<BAKeyInDataViewModel>();
+            //    }
+
+            //    return Json(BAKeyInListView);
+            //}
+            //else
+            //{
+            //    return Json(BAKeyInListView);
+            //}
+
+
+
+        //}
+
+        #endregion
 
     }
 }
