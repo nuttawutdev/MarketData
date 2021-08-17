02fc68f4a7a572aba5fe1825d55e927375a460e2
Nuttawut <developernuttawut@gmail.com>
2021-08-17 20:53:28 +0700

    Get adjust list controller

---
+++ MarketData/Controllers/AdjustController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketData.Model.Request.Adjust;
+using MarketData.Models;
+using MarketData.Processes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketData.Controllers
+{
+    public class AdjustController : Controller
+    {
+        private readonly Process process;
+        public AdjustController(Process process)
+        {
+            this.process = process;
+        }
+
+        [HttpPost]
+        public IActionResult GetAdjustList([FromBody] GetAdjustListRequest request)
+        {
+            AdjustListViewModel dataModel = new AdjustListViewModel();
+
+            try
+            {              
+                return Json(dataModel);
+            }
+            catch (Exception ex)
+            {
+                return Json(dataModel);
+            }
+        }
+    }
+}
---
+++ MarketData/Models/AdjustListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketData.Models
+{
+    public class AdjustListViewModel
+    {
+        public List<AdjustViewData> data { get; set; }
+        public List<string> columnList { get; set; }
+    }
+
+    public class AdjustViewData
+    {
+        public string year { get; set; }
+        public string month { get; set; }
+        public string week { get; set; }
+        public string counter { get; set; }
+        public Guid retailerGroupID { get; set; }
+        public string retailerGroupName { get; set; }
+        public Guid departmentStoreID { get; set; }
+        public string departmentStoreName { get; set; }
+        public Guid distributionChannelID { get; set; }
+        public string distributionChannelName { get; set; }
+        public Guid statusID { get; set; }
+        public string statusName { get; set; }
+        public Dictionary<string, string> brandStatus { get; set; }
+
+    }
+}
